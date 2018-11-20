aa59261e4ccdd91c0feef2eb646539e8c9527ab9
Mark Andersen <31654229+MarkiiMedia@users.noreply.github.com>
2018-11-20 10:31:07 +0100

    scroll to top button lavet
    lavet scroll to top button

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,4 +11,20 @@ $(document).ready(function() {
 		scrollTop: position
 	} , 500, 'linear'); //Her kan f.eks tallet 500 ændres så scroll effekten bliver langsommere eller hurtigere
 });
+	
+	// Return to top herunder
+	
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 1000);
+});
+	
 		});
